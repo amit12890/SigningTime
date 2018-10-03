111523f4dce409b92f42be81ebe49096f4e2970a
Doug <doug.garding@outlook.com>
2018-10-02 20:32:18 -0600

    Minor changes trying to figure out how to get it to work. Never did.

--- SigningTime/SigningTime/SignDemonstration.xaml.cs
+++ SigningTime/SigningTime/SignDemonstration.xaml.cs
@@ -2,14 +2,12 @@
 using Plugin.MediaManager.Abstractions.Enums;
 using Plugin.MediaManager.Abstractions.Implementations;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection; // For debugging the file system
 
 using Xamarin.Forms;
-using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
 
@@ -18,6 +16,9 @@ namespace SigningTime
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SignDemonstration : ContentPage
     {
+
+        string internetURL = "https://ia800201.us.archive.org/12/items/BigBuckBunny_328/BigBuckBunny_512kb.mp4";
+
         public SignDemonstration(Sign tappedSign)
         {
             if (tappedSign == null)
@@ -33,11 +34,32 @@ namespace SigningTime
         // https://youtu.be/luDyX0kYzY4?t=7m40s "The file could be embedded in your project."
         private async Task Button_ClickedAsync(object sender, EventArgs e)
         {
+
+            // use for debugging, not in released app code!
+            // Check in the Application Output to see if files are being placed there
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(SigningTime.SignDemonstration)).Assembly;
+            foreach (var res in assembly.GetManifestResourceNames())
+            {
+                System.Diagnostics.Debug.WriteLine("found resource: " + res.GetType());
+            }
+
             // Gets the path to the video file.
-            string pathToFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "SigningTime.video.banana.mp4");
+            string appDirectory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string solutionPath = "SigningTime.video.cracker.mp4";
+            string pathToFile = Path.Combine(appDirectory, solutionPath);
+
+            var video = new MediaFile
+            {
+                Type = MediaFileType.Audio,
+                Availability = ResourceAvailability.Remote,
+                Url = pathToFile
+            };
+
 
             // Creates new MediaFile object, adds it to the queue and starts playing
-            await CrossMediaManager.Current.Play("file://" + pathToFile, MediaFileType.Video, ResourceAvailability.Local);
+            await CrossMediaManager.Current.Play(video);
+            // await CrossMediaManager.Current.Play(pathToFile, MediaFileType.Video, ResourceAvailability.Remote);
+            // await CrossMediaManager.Current.Play(internetURL, MediaFileType.Video, ResourceAvailability.Remote);
         }
     }
 }
