d1480006bbb73e19ce90222aec5bf289a344c6ca
Doug <doug.garding@outlook.com>
2018-11-28 15:45:53 -0700

    Flash card code refactored to follow better design patterns. Now smoother

--- SigningTime/SigningTime/FlashCard.xaml.cs
+++ SigningTime/SigningTime/FlashCard.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-
 using Octane.Xamarin.Forms.VideoPlayer; // For the video player
 using Octane.Xamarin.Forms.VideoPlayer.Events;
-using Octane.Xamarin.Forms.VideoPlayer.Constants;
 using Xamarin.Forms;
 
 namespace SigningTime
@@ -12,7 +10,7 @@ namespace SigningTime
 
         private Sign sign;
         private bool front; // Represents if card front is facing out
-        private VideoPlayer videoPlayer;
+        private bool cardInVideoMode;
 
         // Parameterless constructor for building the layout
         public FlashCard()
@@ -27,11 +25,28 @@ namespace SigningTime
             front = true;
             InitializeComponent();
 
-            // Sets up the current Page with correct (cleared out) values
+            // Sets up the card's Views with correct values
             currentCardNumber.Text = "(" + cardNumber + "/" + numOfCards + ")";
-            signName.Text = "";
-            signDescription.Text = "";
+            signName.Text = sign.Name;
+            signDescription.Text = sign.Description;
+            signImage.Source = sign.Name;
+
+            // Hide/Display the correct views for the card's front
+            signName.IsVisible = false;
+            signDescription.IsVisible = false;
+            signImage.IsVisible = true;
             videoButton.IsVisible = false;
+
+            // Set up the VideoPlayer object
+            videoPlayer.Source = VideoSource.FromResource(sign.Name + ".mp4");
+            // Registers what to do with the video player based on video state
+            videoPlayer.Completed += (object sender, VideoPlayerEventArgs e) => {
+                ResetAndHideVideo();
+            };
+
+            // Add the video to the layout
+            outerLayout.Children.Add(videoPlayer);
+            outerLayout.RaiseChild(videoPlayer);
 
             // Can't have a resoruce image file with name "new", so 
             // add '_' to the name for the img file
@@ -46,11 +61,28 @@ namespace SigningTime
         }
 
         /// <summary>
-        /// Launches the SignDemonstration page for this sign
+        /// Begins playing the demonstration video associated with this sign.
+        /// 
+        /// The FlashCards are set up with two Grids, one on top of the other. 
+        /// This allows for the easy positioning of the video player right on 
+        /// top of the underlying "card."
+        /// 
+        /// The underlying static image of the sign as well as its descriptive
+        /// text is hidden from view, and the VideoPlayer is revealed. As
+        /// defined in the cosntructor, the VideoPlayer will hide and the static 
+        /// information will reappear once the video terminates.
+        /// 
         /// </summary>
-        private void SignDemonstration(object sender, System.EventArgs e)
+        private void DemonstrateSign(object sender, System.EventArgs e)
         {
-            addVideoToCard();
+            // Hide underlying text and static image
+            signDescription.Opacity = 0.1;
+            signImage.Opacity = 0.1;
+
+            // Set up and start the video
+            cardInVideoMode = true;
+            videoPlayer.IsVisible = true;
+            videoPlayer.Play();
         }
 
         /// <summary>
@@ -64,8 +96,7 @@ namespace SigningTime
         {
             uint speed = 400;
 
-            if (videoPlayer != null)
-            {
+            if(cardInVideoMode){
                 videoPlayer.Pause();
             }
 
@@ -73,35 +104,38 @@ namespace SigningTime
             await outerLayout.RotateYTo(-90, speed, Easing.SpringIn);
             outerLayout.RotationY = -270;
 
-            // Swaps out what's present on the card
+            // Switching card to the back
             if (front)
-            { // Currently on front, so flip to back
+            {
                 front = false;
+                signName.IsVisible = true;
+                videoButton.IsVisible = true;
 
-                if (videoPlayer != null)
+                if (cardInVideoMode)
                 {
                     videoPlayer.IsVisible = true;
-                    signDescription.IsVisible = false;
-                    signImage.IsVisible = false;
+                    signDescription.Opacity = 0.1;
+                    signImage.Opacity = 0.1;
                 }
-
-                signName.Text = sign.Name;
-                signDescription.Text = sign.Description;
-                videoButton.IsVisible = true;
+                else
+                {
+                    signDescription.IsVisible = true;
+                    signDescription.Opacity = 1;
+                    signImage.IsVisible = true;
+                    signImage.Opacity = 1 ;
+                }
             }
+            // Switching card to the front
             else
             {
-                front = true; // Currently on back, so flip to front
-                signName.Text = "";
-                signDescription.Text = "";
+                front = true;
+                signImage.IsVisible = true;
+                signImage.Opacity = 1;
+                signDescription.Opacity = 1;
+                signName.IsVisible = false;
+                signDescription.IsVisible = false;
                 videoButton.IsVisible = false;
-
-                if (videoPlayer != null)
-                {
-                    videoPlayer.IsVisible = false;
-                    signDescription.IsVisible = true;
-                    signImage.IsVisible = true;
-                }
+                videoPlayer.IsVisible = false;
             }
 
             // Continues rotating the card the remaining 90 degrees
@@ -110,55 +144,29 @@ namespace SigningTime
         }
 
         /// <summary>
-        /// Adds a video player to this flash card page. For visual clarity,
-        /// the underlying static image of the sign as well as its descriptive
-        /// text is hidden from view, so as to not clutter the card. The video
-        /// will begin playing immediately.
+        /// Hides the VideoPlayer from view and resets the video source.
         /// 
-        /// The FlashCards are set up with two Grids, one on top of the other. 
-        /// This allows for the easy positioning of the video player right on 
-        /// top of the underlying "card." 
+        /// Video playback was having some issues where if the video completed,
+        /// fired the lambda function that hides the video, then the user clicked
+        /// the video display/play button again, the VideoPlayer would flicker
+        /// into view and then dissapear without playing anything. Resetting 
+        /// the source of the video seems to reset whatever was causing
+        /// the issue to occur.
         /// </summary>
-        private void addVideoToCard()
+        internal void ResetAndHideVideo()
         {
-
-            // Set up the VideoPlayer object
-            videoPlayer = new VideoPlayer
-            {
-                AutoPlay = true,
-                DisplayControls = false,
-                Source = VideoSource.FromResource(sign.Name + ".mp4"),
-                VerticalOptions = LayoutOptions.CenterAndExpand,
-                BackgroundColor = new Color(254, 247, 221)
-            };
-
-            // Registers what to do with the video player based on video state
-            // videoPlayer.PlayerStateChanged += testTwo;
-            videoPlayer.Completed += (object sender, VideoPlayerEventArgs e) => {
-                removeVideoFromCard();
-            };
-
-            // Hide underlying views (static image of sign and descriptive text)
-            signDescription.IsVisible = false;
-            signImage.IsVisible = false;
-
-            // Add the video to the layout
-            outerLayout.Children.Add(videoPlayer);
-            outerLayout.RaiseChild(videoPlayer);
-        }
-
-
-        internal void removeVideoFromCard()
-        {
-            if(videoPlayer == null){
-                return;
-            }
-
-            // Hide underlying views (static image of sign and descriptive text)
+            // Set video parameters
+            cardInVideoMode = false;
             videoPlayer.Pause();
+            videoPlayer.Source = VideoSource.FromResource(sign.Name + ".mp4");
             videoPlayer.IsVisible = false;
-            signDescription.IsVisible = true;
-            signImage.IsVisible = true;
+
+            // Set image/text parameters
+            signImage.Opacity = 1;
+            if (!front)
+            {
+                signDescription.Opacity = 1;
+            }
         }
     }
 }
--- SigningTime/SigningTime/FlashCardCarousel.xaml.cs
+++ SigningTime/SigningTime/FlashCardCarousel.xaml.cs
@@ -12,36 +12,36 @@ namespace SigningTime
 
             // The list of signs
             List<Sign> allSigns = new List<Sign>(){
-                new Sign { Name = "again" },
+                new Sign { Name = "again", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
                 new Sign { Name = "baby", Description = "Pretend that you're cradling and rocking a baby." },
-                new Sign { Name = "banana" },
+                new Sign { Name = "banana", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
                 new Sign { Name = "bird", Description = "Open and close your pointer finger a few times to make a bird's beak." },
-                new Sign { Name = "car" },
+                new Sign { Name = "car", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
                 new Sign { Name = "cat", Description = "Brush your closed pointer finger and thum away from your cheek one or two times, like you're stroking a cat's whisker." },
-                new Sign { Name = "cereal" },
-                new Sign { Name = "cracker" },
+                new Sign { Name = "cereal", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
+                new Sign { Name = "cracker", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
                 new Sign { Name = "dad", Description = "Put the thumb of your open hand on your forehead."},
-                new Sign { Name = "diaper" },
+                new Sign { Name = "diaper", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
                 new Sign { Name = "dog", Description = "Pat your let, or pat and snap like you're calling a dog." },
-                new Sign { Name = "dream" },
-                new Sign { Name = "drink" },
+                new Sign { Name = "dream", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
+                new Sign { Name = "drink", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
                 new Sign { Name = "eat", Description = "Tap your fingers to your mouth a few times, just like you're going to eat something. It's also the sign for food." },
-                new Sign { Name = "finished" },
-                new Sign { Name = "fish" },
+                new Sign { Name = "finished", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
+                new Sign { Name = "fish", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
                 new Sign { Name = "frog", Description = "Flick your first two fingers out a few times, like frog legs hopping." },
                 new Sign { Name = "grandma", Description = "Put the thumb of your open hand on your chin, just like \"mom,\" but bounce your hand forward"},
                 new Sign { Name = "grandpa", Description = "Put the thumb of your open hand on your forehead, just like \"dad,\" but bounce your hand forward"},
-                new Sign { Name = "horse" },
-                new Sign { Name = "hurt" }, // LONGER THAN 15sec
-                new Sign { Name = "juice" },
-                new Sign { Name = "love" },
+                new Sign { Name = "horse", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
+                new Sign { Name = "hurt", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." }, // LONGER THAN 15sec
+                new Sign { Name = "juice", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
+                new Sign { Name = "love", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
                 new Sign { Name = "milk", Description = "Squeeze and relax your fist a few times, like you're milking a cow." },
-                new Sign { Name = "mom" },
-                new Sign { Name = "more" },
-                new Sign { Name = "new" },
+                new Sign { Name = "mom", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
+                new Sign { Name = "more", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
+                new Sign { Name = "new", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
                 new Sign { Name = "potty", Description = "Tuck your thumb between your pointer and middle fingers, and shake it a little." },
-                new Sign { Name = "water" },
-                new Sign { Name = "where" }, // TODO: NEED THIS VIDEO DOWNLOADED
+                new Sign { Name = "water", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." },
+                new Sign { Name = "where", Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor incididunt ut labore et dolore magna aliqua." }, // TODO: NEED THIS VIDEO DOWNLOADED
             };
 
             InitializeComponent();
@@ -93,14 +93,14 @@ namespace SigningTime
             int indexOfCurrentPage = this.Children.IndexOf(CurrentPage);
 
             if (indexOfCurrentPage == 0){
-                ((FlashCard)Children[1]).removeVideoFromCard();
+                ((FlashCard)Children[1]).ResetAndHideVideo();
             }
             else if(indexOfCurrentPage == Children.Count-1){
-                ((FlashCard)Children[Children.Count - 2]).removeVideoFromCard();
+                ((FlashCard)Children[Children.Count - 2]).ResetAndHideVideo();
             }
             else{
-                ((FlashCard)Children[indexOfCurrentPage - 1]).removeVideoFromCard();
-                ((FlashCard)Children[indexOfCurrentPage + 1]).removeVideoFromCard();
+                ((FlashCard)Children[indexOfCurrentPage - 1]).ResetAndHideVideo();
+                ((FlashCard)Children[indexOfCurrentPage + 1]).ResetAndHideVideo();
             }
         }
 
