f53fe7af7d4e3ac8d322da05c3df57240849fd31
Doug <doug.garding@outlook.com>
2019-01-27 17:09:55 -0700

    The app will now remember the same list across all Pages
    Tweaks were made so that when a user turns a card on or off (for use in a custom flash card deck), the app will now remember the status of each card. However, this isn't remembered when the app is closed, so this data needs to be saved more deeply.

--- SigningTime/SigningTime/App.xaml.cs
+++ SigningTime/SigningTime/App.xaml.cs
@@ -8,6 +8,34 @@ namespace SigningTime
 {
     public partial class App : Application
     {
+        // APP-WIDE GLOBAL LIST
+        public static List<Sign> allSigns = new List<Sign>(){
+            new Sign { Name = "baby", UseAsFlashCard = true, Description = "Pretend that you're cradling and rocking a baby." },
+            new Sign { Name = "banana", UseAsFlashCard = true, Description = "Point one finger up and pretend you're peeling a banana." },
+            new Sign { Name = "bird", UseAsFlashCard = true, Description = "Open and close your pointer finger a few times to make a bird's beak." },
+            new Sign { Name = "cat", UseAsFlashCard = true, Description = "Brush your closed pointer finger and thum away from your cheek one or two times, like you're stroking a cat's whisker." },
+            new Sign { Name = "cereal", UseAsFlashCard = true, Description = "Wiggle your bent pointer finger across your chin." },
+            new Sign { Name = "cracker",UseAsFlashCard = true, Description = "Knock your fist on your elbow a few times." },
+            new Sign { Name = "dad", UseAsFlashCard = true, Description = "Put the thumb of your open hand on your forehead."},
+            new Sign { Name = "diaper", UseAsFlashCard = true, Description = "Tap your first two fingers against your thumbs twice, right where you fasten a diaper." },
+            new Sign { Name = "dog", UseAsFlashCard = true, Description = "Pat your leg, or pat and snap like you're calling a dog." },
+            new Sign { Name = "drink", UseAsFlashCard = true, Description = "Pretend to drink from a cup." },
+            new Sign { Name = "eat", UseAsFlashCard = true, Description = "Tap your fingers to your mouth a few times, just like you're going to eat something. It's also the sign for food." },
+            new Sign { Name = "finished", UseAsFlashCard = true, Description = "Twist your hands back and forth a few times, like you are brushing everything away. It’s also the sign for all done." },
+            new Sign { Name = "fish", UseAsFlashCard = true, Description = "Wiggle your hand away from you like a fish swimming." },
+            new Sign { Name = "frog", UseAsFlashCard = true, Description = "Rest your chin on your fist and flick your first two fingers out a few times, like frog legs hopping." },
+            new Sign { Name = "grandma", UseAsFlashCard = true, Description = "Put the thumb of your open hand on your chin, just like \"mom,\" but bounce your hand forward"},
+            new Sign { Name = "grandpa", UseAsFlashCard = true, Description = "Put the thumb of your open hand on your forehead, just like \"dad,\" but bounce your hand forward"},
+            new Sign { Name = "horse", UseAsFlashCard = true, Description = "Touch your thumb to the side of your head and wave your first two fingers forward a few times, like a horse’s ear." },
+            new Sign { Name = "hurt", UseAsFlashCard = true, Description = "Push your pointer fingers together over the top of your owie." }, // LONGER THAN 15sec
+            new Sign { Name = "juice", UseAsFlashCard = true, Description = "Use your pinky finger to draw a \"J\" for Juice." },
+            new Sign { Name = "milk", UseAsFlashCard = true, Description = "Squeeze and relax your fist a few times, like you're milking a cow." },
+            new Sign { Name = "mom", UseAsFlashCard = true, Description = "Put the thumb of your open hand on your chin." },
+            new Sign { Name = "more", UseAsFlashCard = true, Description = "Tap your closed fingertips together a few times." },
+            new Sign { Name = "potty", UseAsFlashCard = true, Description = "Tuck your thumb between your pointer and middle fingers, and shake it a little." },
+            new Sign { Name = "water", UseAsFlashCard = true, Description = "Make a \"W\" with your three fingers, and tap it at your chin a few times." },
+            new Sign { Name = "where", UseAsFlashCard = true, Description = "With your pointer finger up, shake your hand from side to side, like you are not sure where to point." }
+        };
 
         public App()
         {
@@ -31,8 +59,6 @@ namespace SigningTime
                 // No custom renderer needed for Android
                 MainPage = new Tabs();
             }
-
-
         }
 
         protected override void OnStart()
--- SigningTime/SigningTime/CustomizeDeck.xaml.cs
+++ SigningTime/SigningTime/CustomizeDeck.xaml.cs
@@ -10,49 +10,20 @@ namespace SigningTime
 {
     public partial class CustomizeDeck : ContentPage
     {
-        // The list of signs
-        List<Sign> listOfSigns = new List<Sign>(){
-            new Sign { Name = "baby", Description = "Pretend that you're cradling and rocking a baby.", UseAsFlashCard = true },
-            new Sign { Name = "banana", Description = "Point one finger up and pretend you're peeling a banana.", UseAsFlashCard = true },
-            new Sign { Name = "bird", Description = "Open and close your pointer finger a few times to make a bird's beak.", UseAsFlashCard = true },
-            new Sign { Name = "cat", Description = "Brush your closed pointer finger and thum away from your cheek one or two times, like you're stroking a cat's whisker.", UseAsFlashCard = true },
-            new Sign { Name = "cereal", Description = "Wiggle your bent pointer finger across your chin.", UseAsFlashCard = true },
-            new Sign { Name = "cracker", Description = "Knock your fist on your elbow a few times.", UseAsFlashCard = true },
-            new Sign { Name = "dad", Description = "Put the thumb of your open hand on your forehead.", UseAsFlashCard = true},
-            new Sign { Name = "diaper", Description = "Tap your first two fingers against your thumbs twice, right where you fasten a diaper.", UseAsFlashCard = true },
-            new Sign { Name = "dog", Description = "Pat your leg, or pat and snap like you're calling a dog.", UseAsFlashCard = true },
-            new Sign { Name = "drink", Description = "Pretend to drink from a cup.", UseAsFlashCard = true },
-            new Sign { Name = "eat", Description = "Tap your fingers to your mouth a few times, just like you're going to eat something. It's also the sign for food.", UseAsFlashCard = true },
-            new Sign { Name = "finished", Description = "Twist your hands back and forth a few times, like you are brushing everything away. It’s also the sign for all done.", UseAsFlashCard = true },
-            new Sign { Name = "fish", Description = "Wiggle your hand away from you like a fish swimming.", UseAsFlashCard = true },
-            new Sign { Name = "frog", Description = "Rest your chin on your fist and flick your first two fingers out a few times, like frog legs hopping.", UseAsFlashCard = true },
-            new Sign { Name = "grandma", Description = "Put the thumb of your open hand on your chin, just like \"mom,\" but bounce your hand forward", UseAsFlashCard = true},
-            new Sign { Name = "grandpa", Description = "Put the thumb of your open hand on your forehead, just like \"dad,\" but bounce your hand forward", UseAsFlashCard = true},
-            new Sign { Name = "horse", Description = "Touch your thumb to the side of your head and wave your first two fingers forward a few times, like a horse’s ear.", UseAsFlashCard = true },
-            new Sign { Name = "hurt", Description = "Push your pointer fingers together over the top of your owie.", UseAsFlashCard = true }, // LONGER THAN 15sec
-            new Sign { Name = "juice", Description = "Use your pinky finger to draw a \"J\" for Juice.", UseAsFlashCard = true },
-            new Sign { Name = "milk", Description = "Squeeze and relax your fist a few times, like you're milking a cow.", UseAsFlashCard = true },
-            new Sign { Name = "mom", Description = "Put the thumb of your open hand on your chin.", UseAsFlashCard = true },
-            new Sign { Name = "more", Description = "Tap your closed fingertips together a few times.", UseAsFlashCard = true },
-            new Sign { Name = "potty", Description = "Tuck your thumb between your pointer and middle fingers, and shake it a little.", UseAsFlashCard = true },
-            new Sign { Name = "water", Description = "Make a \"W\" with your three fingers, and tap it at your chin a few times.", UseAsFlashCard = true },
-            new Sign { Name = "where", Description = "With your pointer finger up, shake your hand from side to side, like you are not sure where to point.", UseAsFlashCard = true }
-        };
-
 
         public CustomizeDeck()
         {
             InitializeComponent();
 
             // Adds all the signs to the grid
-            for (int row = 0; row < listOfSigns.Count ; row+=3)
+            for (int row = 0; row < SignList.allSigns.Count ; row+=3)
             {
 
                 // Adds a full set of signs to the current row
                 for (int column = 0; column < 3; column++)
                 {
                     // Stop filling out this row if we're out of signs
-                    if (row + column+1 > listOfSigns.Count)
+                    if (row + column+1 > SignList.allSigns.Count)
                     {
                         break;
                     }
@@ -63,7 +34,7 @@ namespace SigningTime
                     gridOfSigns.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
 
                     // Add a border/frame to the layout
-                    StackLayout stackLayout = GenerateSignButton(listOfSigns[row + column].Name);
+                    StackLayout stackLayout = GenerateSignButton(App.allSigns[row + column].Name);
 
                     Frame frame = new Frame {
                         BackgroundColor = Color.FromHex("#f48d91"),
@@ -73,6 +44,12 @@ namespace SigningTime
                         CornerRadius = 5,
                         Margin = 5,
                     };
+
+                    // Set opacity based on whether card is set to be used in the deck or not
+                    if (!App.allSigns[row + column].UseAsFlashCard)
+                    {
+                        frame.Opacity = 0.4;
+                    }
 
                     // Create a gesture recognizer for when a user selects a sign
                     var tapGestureRecognizer = new TapGestureRecognizer();
@@ -85,20 +62,19 @@ namespace SigningTime
                         String nameOfSign = signLabel.Text;
 
                         // Edit the sign's "UseAsFlashcard" parameter in the array of signs
-                        for(int i = 0; i < listOfSigns.Count; i++)
+                        for(int i = 0; i < App.allSigns.Count; i++)
                         {
                             // Find the sign
-                            Sign currentSign = listOfSigns[i];
-                            if (currentSign.Name.Equals(nameOfSign))
+                            if (App.allSigns[i].Name.Equals(nameOfSign))
                             {
                                 // Determine if the card is set to be used or not
-                                if (currentSign.UseAsFlashCard)
+                                if (App.allSigns[i].UseAsFlashCard)
                                 {
-                                    currentSign.UseAsFlashCard = false;
+                                    App.allSigns[i].UseAsFlashCard = false;
                                 }
                                 else
                                 {
-                                    currentSign.UseAsFlashCard = true;
+                                    App.allSigns[i].UseAsFlashCard = true;
                                 }
                                 // End the for loop
                                 break;
@@ -170,10 +146,10 @@ namespace SigningTime
         {
             // Copy the selected signs into a list for the carousel
             List<Sign> newList = new List<Sign>();
-            for(int i = 0; i < listOfSigns.Count; i++){
-                if (listOfSigns[i].UseAsFlashCard)
+            for(int i = 0; i < App.allSigns.Count; i++){
+                if (App.allSigns[i].UseAsFlashCard)
                 {
-                    newList.Add(listOfSigns[i]);
+                    newList.Add(App.allSigns[i]);
                 }
             }
 
